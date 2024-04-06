2cc4ba533aa303832ece497743994d5d7326974f
Aboud Virus.Net <51423974+AboudVirus@users.noreply.github.com>
2024-04-06 05:02:21 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -19,6 +19,6 @@ app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-//client.login(process.env.token)
+
 module.exports = app;
 module.exports.handler = serverless(app);
