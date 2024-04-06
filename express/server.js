c080ea572b7eedea4ba5ace10b23587cc598bd1a
Aboud Virus.Net <51423974+AboudVirus@users.noreply.github.com>
2024-04-07 01:44:00 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -12,6 +12,16 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+router.get('/v1', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>v1 from Express.js!</h1>');
+  res.end();
+});
+router.get('/v1/00', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>v1/00 from Express.js!</h1>');
+  res.end();
+});
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
